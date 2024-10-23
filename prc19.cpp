0256761e2e1726097222b2d3814fc8235cba1915
ZephyrBD <lxy2984406508@outlook.com>
2024-10-23 15:18:20 +0000

    解决初中编程问题
    Signed-off-by: ZephyrBD <lxy2984406508@outlook.com>

---
+++ prc19.cpp
@@ -0,0 +1,54 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+int search(int rnum[],int num,int len);
+
+int main()
+{
+	int n;
+	cin >> n;
+	srand((unsigned int)time(NULL));
+	int num = 0;
+	int rnum[n] = {0};
+	int x = 0;
+	int len = 0;
+	rnum[0] = num = (rand() % 1000) + 1;
+	while(x<n)
+	{
+		num = (rand() % 100) + 1;
+		len = sizeof(rnum)/sizeof(rnum[0]);
+		if(search(rnum,num,len))
+		{
+			rnum[x] = num;
+			x++;
+			cout << num << " ";
+		}
+	}
+	for(int i=0;i<n;i++){
+		for(int j=0;j<n-1-i;j++)
+		{
+			if(rnum[j] > rnum[j+1])
+			{
+				swap(rnum[j],rnum[j+1]);
+			}
+		}
+	}
+	cout << endl;
+	for(int i=0;i<n;i++)
+	{
+		cout << rnum[i] << " ";
+	}
+	system("pause");
+	return 0;
+}
+
+int search(int rnum[],int num,int len)
+{
+	int key = 1;
+	for(int i=0;i<len;i++){
+		if(rnum[i] == num){
+			key = i;
+		}
+	}
+	return key;
+}
